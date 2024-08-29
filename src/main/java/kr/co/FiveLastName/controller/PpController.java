ea203496525b496ebccf65d39e7867130e2fa713
HugoHan <eohan1014@gmail.com>
2024-08-29 09:42:09 +0900

    마스터 pull

--- src/main/java/kr/co/FiveLastName/controller/PpController.java
+++ src/main/java/kr/co/FiveLastName/controller/PpController.java
@@ -1,8 +1,18 @@
 package kr.co.FiveLastName.controller;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.FiveLastName.service.PpService;
 
 @Controller
 public class PpController {
+		
+	@Inject
+	PpService ppService;
+	
+	@GetMapping(value="/")
 
 }
