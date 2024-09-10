94e517e01fa750380a1ca281e3b3ef437751b770
Kim-Hyeong-Mo <independence1919@gmail.com>
2024-09-10 18:08:39 +0900

    9월10일 작업 업로드 HM

---
+++ src/main/java/kr/co/FiveLastName/service/TransactionStatementService.java
@@ -0,0 +1,22 @@
+package kr.co.FiveLastName.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.FiveLastName.domain.TransactionStatementDTO;
+
+@Service
+public interface TransactionStatementService {
+	// 거래명세서 조회
+		public List<TransactionStatementDTO> ts_list();
+		
+//		거래명세서 상세 조회
+		public TransactionStatementDTO ts_selectOne(int ts_num);
+		
+//		거래명세서 등록
+		public int ts_insert(TransactionStatementDTO tsDTO);
+		
+//		거래명세서 수정
+		public int ts_update(TransactionStatementDTO tsDTO);
+}
